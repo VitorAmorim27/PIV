7011bcfd828ea11835ba17edbaeb139ca3b3680c
VitorAmorim27 <79467789+VitorAmorim27@users.noreply.github.com>
2024-04-22 00:01:31 +0100

    menu

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
-var holeX
-var holeY
-var x
-var y
-var xSpeed
-var ySpeed
-var strokes
+var holeX;
+var holeY;
+var x;
+var y;
+var xSpeed;
+var ySpeed;
+var strokes;
 var treeImage;
-var counter
-var strokeDistance
-var radioSticks; 
+var counter;
+var strokeDistance;
+var radioSticks;
 var nivelTexto = 'Nivel 1';
+var jogoIniciado = false;
 
 var niveis = {
   'Nivel 1': {
@@ -41,6 +42,42 @@ function setup() {
   // Cria o canvas
   createCanvas(900, 600);
 
+  // Cria elementos do menu inicial
+  criarMenuInicial();
+}
+
+function draw() {
+  if (jogoIniciado) {
+    // Se o jogo já foi iniciado, desenhe o jogo
+    desenharJogo();
+  } else {
+    // Se o jogo ainda não foi iniciado, desenhe o menu inicial
+    desenharMenuInicial();
+  }
+}
+
+// Função para criar elementos do menu inicial
+function criarMenuInicial() {
+  // Define o plano de fundo como verde
+  background(0, 220, 0);
+
+  // Texto do título
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text("Menu Inicial", width / 2, height / 2 - 50);
+
+  // Botão para iniciar o jogo
+  button = createButton('Iniciar Jogo');
+  button.position(width / 2 - 50, height / 2);
+  button.size(100, 50);
+  button.mousePressed(iniciarJogo);
+}
+
+// Função para iniciar o jogo
+function iniciarJogo() {
+  jogoIniciado = true;
+
   // Cria o grupo de botões de opção para selecionar os níveis
   radioNivel = createRadio();
   radioNivel.position(15, height - 75); // Posição no canto inferior esquerdo
@@ -58,9 +95,13 @@ function setup() {
   initializeGame(nivelAtual);
 }
 
+// Função para desenhar o menu inicial
+function desenharMenuInicial() {
+  // Não é necessário desenhar nada no draw(), pois o menu inicial já foi desenhado no setup()
+}
+
 // Função para inicializar o jogo com base nas configurações do nível escolhido
 function initializeGame(nivel) {
-  
   holeX = nivel.holeX;
   holeY = nivel.holeY;
   x = nivel.ballX;
@@ -83,8 +124,6 @@ function initializeGame(nivel) {
   radioSticks.option('Taco 3');
 
   radioSticks.changed(changeStick);
-  // Outros parâmetros do jogo
-  // Exemplo: adicionar obstáculos ao campo com base no nível
 }
 
 // Função chamada quando o nível é alterado
@@ -96,116 +135,114 @@ function changeNivel() {
   initializeGame(nivelAtual);
 }
 
-
-
-function draw() {
+// Função para desenhar o jogo
+function desenharJogo() {
   // Define o plano de fundo como verde
   background(0, 220, 0);
-  
+
   // Desenha um retângulo ao redor do canvas
   stroke(0, 0, 0); // Cor da linha preta
   strokeWeight(20); // Espessura da linha
   noFill(); // Não preenche o retângulo
   rect(0, 0, width, height); // Retângulo ao redor do canvas
-  
+
   // Mantém o buraco em sua posição atual
   fill(0, 0, 0);
   ellipse(holeX, holeY, 30);
-  
+
   // Desenha a bola de golfe
   stroke(0, 0, 0); // Cor da linha preta
   strokeWeight(2); // Espessura da linha para a bola
   fill(255); // Cor branca para a bola de golfe
   ellipse(x, y, 20);
 
-// Verifica se o nível atual é o nível 1 e se a bola colidiu com o lago
-if (nivelTexto === 'Nivel 1' && collidePointEllipse(x, y, 350, 350, 180, 80)) {
+  // Verifica se o nível atual é o nível 1 e se a bola colidiu com o lago
+  if (nivelTexto === 'Nivel 1' && collidePointEllipse(x, y, 350, 350, 180, 80)) {
     // Se a bola colidir com o lago, calcule a direção da bola em relação ao centro do lago
     var dx = x - 350; // Diferença na posição x entre a bola e o centro do lago
     var dy = y - 350; // Diferença na posição y entre a bola e o centro do lago
     var distance = dist(x, y, 350, 350); // Distância entre a bola e o centro do lago
-    
+
     // Calcule os novos valores de x e y para posicionar a bola a 40 unidades do centro do lago
     x = 350 + (dx / distance) * (180 / 2 + 40);
     y = 350 + (dy / distance) * (80 / 2 + 40);
-    
+
     // Zere as velocidades da bola
     xSpeed = 0;
     ySpeed = 0;
-    
+
     // Aumenta o contador de jogadas
     strokes++;
-}
-
+  }
 
   // Se a bola de golfe cair no buraco
   if (abs(x - holeX) <= 5 && abs(y - holeY) <= 5) {
-      xSpeed = 0;
-      ySpeed = 0;
-      fill(0, 0, 255);
-      textSize(32);
-      text('YOU WIN!', 10, 30);
-      if (strokes == 1) {
-          text('Hole in One!', 10, 70);
-      } else {
-          text(strokes + ' strokes!', 10, 70);
-      }
+    xSpeed = 0;
+    ySpeed = 0;
+    fill(0, 0, 255);
+    textSize(32);
+    text('YOU WIN!', 10, 30);
+    if (strokes == 1) {
+      text('Hole in One!', 10, 70);
+    } else {
+      text(strokes + ' strokes!', 10, 70);
+    }
   } else if (counter > 10 * strokeDistance) {
-      // Para a bola e reseta o counter
-      xSpeed = 0;
-      ySpeed = 0;
-      counter = 0;
+    // Para a bola e reseta o counter
+    xSpeed = 0;
+    ySpeed = 0;
+    counter = 0;
   } else if (x <= 30 || y <= 30 || x >= width - 30 || y >= height - 30) {
-      // Para a bola e ajusta a posição se necessário
-      xSpeed = 0;
-      ySpeed = 0;
-      // Verifica os limites
-      if (x >= width - 30) {
-          x -= 1;
-      }
-      if (y >= height - 30) {
-          y -= 1;
-      }
-      if (x <= 30) {
-          x += 1;
-      }
-      if (y <= 30) {
-          y += 1;
-      }
+    // Para a bola e ajusta a posição se necessário
+    xSpeed = 0;
+    ySpeed = 0;
+    // Verifica os limites
+    if (x >= width - 30) {
+      x -= 1;
+    }
+    if (y >= height - 30) {
+      y -= 1;
+    }
+    if (x <= 30) {
+      x += 1;
+    }
+    if (y <= 30) {
+      y += 1;
+    }
   } else {
-      // Atualiza a posição da bola de golfe
-      if (strokeDistance > 0) {
-          counter++;
-      }
-      x += xSpeed;
-      y += ySpeed;
+    // Atualiza a posição da bola de golfe
+    if (strokeDistance > 0) {
+      counter++;
+    }
+    x += xSpeed;
+    y += ySpeed;
   }
-  
+
   // Adiciona o texto com as strokes e a distância
   fill(255);
   textSize(16);
   var distance = dist(x, y, holeX, holeY);
-  text('Jogadas: ' + strokes, 15, 30); 
-  text('Distância: ' + distance.toFixed(2), 15, 60); 
-  
+  text('Jogadas: ' + strokes, 15, 30);
+  text('Distância: ' + distance.toFixed(2), 15, 60);
+
   // Adiciona o texto do nível selecionado
   fill(255);
   textSize(18);
   text(nivelTexto, 350, 30);
-  
+
   // Verifica se o nível atual é o 'Nivel 1' e desenha uma elipse azul
   if (nivelTexto === 'Nivel 1') {
-      fill(0, 0, 255); // Cor azul
-      noStroke();
-      ellipse(350, 350, 180, 80);
+    fill(0, 0, 255); // Cor azul
+    noStroke();
+    ellipse(350, 350, 180, 80);
   }
-  
+
   if (xSpeed === 0 && ySpeed === 0) {
-      // Desenha um círculo ao redor da bola para indicar a área de clique
-      stroke(255, 0, 0); // Cor vermelha para o círculo
-      strokeWeight(2); // Espessura da linha
-      noFill(); // Não preencher o círculo
-      ellipse(x, y, 75); // Desenha um círculo com raio de 75 ao redor da bola
+    // Desenha um círculo ao redor da bola para indicar a área de clique
+    stroke(255, 0, 0); // Cor vermelha para o círculo
+    strokeWeight(2); // Espessura da linha
+    noFill(); // Não preencher o círculo
+    ellipse(x, y, 75); // Desenha um círculo com raio de 75 ao redor da bola
   }
 }
 
@@ -229,9 +266,7 @@ function changeStick() {
   ySpeed *= speedMultiplier;
 }
 
-
 function mousePressed() {
-
   // Verifica se o clique ocorreu na área dos botões de opção
   var radioX = radioSticks.elt.offsetLeft;
   var radioY = radioSticks.elt.offsetTop;
@@ -254,7 +289,7 @@ function mousePressed() {
 
   // Verifica se o clique do mouse está próximo à bola de golfe
   if (collidePointEllipse(mouseX, mouseY, x, y, 75, 75)) {
-   
+
     // Calcula a distância entre o clique e a bola
     var strokeX = abs(mouseX - x);
     var strokeY = abs(mouseY - y);
